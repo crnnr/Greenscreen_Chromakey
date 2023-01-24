3e24cc431237feeda62b1051ff9b08fd70963d01
Your Name <christof-renner@t-online.de>
2023-01-24 19:43:50 +0100

    reworked app.py
    reworked app.py image handeling and key out functionality

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import cv2
 import cvzone
 from cvzone.SelfiSegmentationModule import SelfiSegmentation
+<<<<<<< Updated upstream
 
 def change_background(image, segmen, video, th):
     bgimage = cv2.imread(image)
@@ -32,13 +33,39 @@ segmen = SelfiSegmentation()
 
 # Setzen Intialer Threshold
 th = 0.45
+=======
+import os
+
+# video capture and resolution setting
+cap = cv2.VideoCapture(0)
+cap.set(3, 640)
+cap.set(4, 480)
+cap.set(cv2.CAP_PROP_FPS, 60)
+segementor = SelfiSegmentation()
+fpsReader = cvzone.FPS()
+
+#grabing all images in folder
+listImg = os.listdir("./images/")
+print(listImg)
+imgList = []
+for imgPath in listImg:
+    img = cv2.imread(f'./images/{imgPath}')
+    imgList.append(img)
+print(len(imgList))
+
+indexImg = 0
+>>>>>>> Stashed changes
 
 while True:
-    _,frame = video.read()
-    #Main Window Initieren
-    cv2.namedWindow('video', cv2.WINDOW_NORMAL)
-    cv2.imshow("video",frame)
+    success, img = cap.read()
+    imgOut = segementor.removeBG(img, imgList[indexImg], threshold=0.8)
+
+    imgStacked = cvzone.stackImages([img, imgOut], 2, 1)
+    _, imgStacked = fpsReader.update(imgStacked, color=(0, 0, 255))
+
+    cv2.imshow("Image", imgStacked)
     key = cv2.waitKey(1)
+<<<<<<< Updated upstream
     if key == ord('q'):
         th = change_background("1.jpg", segmen, video, th)
     elif key == ord('w'):
@@ -48,3 +75,14 @@ while True:
     elif key == ord('c'):
         break
 
+=======
+#managekeystroke functionality
+    if key == ord('+'):
+     if indexImg < len(imgList)-1:
+        indexImg += 1
+    elif key == ord('-'):
+     if indexImg > 0:
+        indexImg -= 1        
+    elif key == ord('c'):
+        break
+>>>>>>> Stashed changes
