58737ea53fbadd5953466f5250c58966f6e8f559
Chris <87771733+ChrisCh4padia@users.noreply.github.com>
2023-02-02 21:12:10 +0100

    added command line parameter supports

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 import cv2
 import cvzone
 from cvzone.SelfiSegmentationModule import SelfiSegmentation
+import argparse
 import os
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-ns", "--noslide", help="Remove the slider from the application", action="store_true")
+ap.add_argument("-ok", "--onlykeyed", help="Show only the final picture with adjusted background", action="store_true")
+args = vars(ap.parse_args())
 
 cap = cv2.VideoCapture(0)
 cap.set(3, 640)
@@ -21,20 +27,28 @@ print(len(imgList))
 indexImg = 0
 
 cv2.namedWindow("Image", cv2.WINDOW_NORMAL)
+
+if not args["noslide"]:
+    cv2.createTrackbar("Threshold", "Image", 80, 100, lambda x: x)
+
 while True:
     success, img = cap.read()
-    imgOut = segementor.removeBG(img, imgList[indexImg], threshold=0.8)
+    threshold = cv2.getTrackbarPos("Threshold", "Image") / 100 if not args["noslide"] else 0.8
+    imgOut = segementor.removeBG(img, imgList[indexImg], threshold=threshold)
 
-    imgStacked = cvzone.stackImages([img, imgOut], 2, 1)
-    _, imgStacked = fpsReader.update(imgStacked, color=(0, 0, 255))
+    if args["onlykeyed"]:
+        cv2.imshow("Image", imgOut)
+    else:
+        imgStacked = cvzone.stackImages([img, imgOut], 2, 1)
+        _, imgStacked = fpsReader.update(imgStacked, color=(0, 0, 255))
+        cv2.imshow("Image", imgStacked)
 
-    cv2.imshow("Image", imgStacked)
     key = cv2.waitKey(1)
     if key == ord('+'):
-     if indexImg < len(imgList)-1:
-        indexImg += 1
+        if indexImg < len(imgList) - 1:
+            indexImg += 1
     elif key == ord('-'):
-     if indexImg > 0:
-        indexImg -= 1        
+        if indexImg > 0:
+            indexImg -= 1
     elif key == ord('c'):
         break
